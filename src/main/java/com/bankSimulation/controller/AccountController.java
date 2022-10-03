69318c68a2913bfc222cf6e32d89f80f7b1590e1
ebr <clkebr2601@gmail.com>
2022-10-03 22:38:44 +0200

    delete button is completed

--- src/main/java/com/bankSimulation/controller/AccountController.java
+++ src/main/java/com/bankSimulation/controller/AccountController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Controller
 public class AccountController {
@@ -36,6 +38,17 @@ public class AccountController {
     @PostMapping("/create")
     public String create(@ModelAttribute("account") Account account, Model model){
         accountService.createNewAccount(account.getBalance(),new Date(),account.getAccountType(),account.getUserId());
+        return "redirect:/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String getDeleteAccount(@PathVariable("id") UUID id){
+
+        //find the account and change status to DELETED
+        accountService.deleteAccount(id);
+
+        System.out.println(id);
+
         return "redirect:/index";
     }
 
--- src/main/java/com/bankSimulation/service/impl/AccountServiceImpl.java
+++ src/main/java/com/bankSimulation/service/impl/AccountServiceImpl.java
@@ -33,4 +33,9 @@ public class AccountServiceImpl implements AccountService {
     public List<Account> listAllAccount() {
         return accountRepository.findAll();
     }
+
+    @Override
+    public void deleteAccount(UUID id) {
+        accountRepository.findById(id).setAccountStatus(AccountStatus.DELETED);
+    }
 }

68d3bd42b91f28757a69d72c19167417acf8f899
ebr <clkebr2601@gmail.com>
2022-10-23 16:52:06 +0200

    validation updates

--- src/main/java/com/bankSimulation/controller/AccountController.java
+++ src/main/java/com/bankSimulation/controller/AccountController.java
@@ -6,11 +6,13 @@ import com.bankSimulation.model.Transaction;
 import com.bankSimulation.service.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -37,7 +39,11 @@ public class AccountController {
     }
 
     @PostMapping("/create")
-    public String create(@ModelAttribute("account") Account account, Model model){
+    public String create(@Valid @ModelAttribute("account") Account account,BindingResult bindingResult,Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("accountTypes", AccountType.values());
+            return "account/create-account";
+        }
         accountService.createNewAccount(account.getBalance(),new Date(),account.getAccountType(),account.getUserId());
         return "redirect:/index";
     }
